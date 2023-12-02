6c677b7cede04bb34d52e1037ce5e44c2837b6b4
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-12-02 22:31:49 +0200

    add test endpoint

--- nemr.py
+++ nemr.py
@@ -5,3 +5,9 @@ app = Blueprint('nemr', __name__)
 @app.route("/nemr")
 def hello_world():
     return "<h1 style='text-align: center;'>Welcome From Nemr Side</h1>"
+
+
+
+@app.route("/login")
+def hello_world():
+    return "<h1 style='text-align: center;'>Welcome From login</h1>"
