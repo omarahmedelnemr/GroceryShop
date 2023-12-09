6c3b46acc1b96f79f752089bf6658d506d6f3723
Your Nameadham-aboeldahab <adham.aboeldahab@ejust.edu.eg>
2023-12-10 00:32:15 +0200

    second endpoint

--- Developers/adham.py
+++ Developers/adham.py
@@ -20,10 +20,10 @@ def get_by_search():
     try:
         keyword = request.args.get('keyword', '')
 
-        # Create a cursor object to interact with the database
+       
         cursor = myDB.cursor(dictionary=True)
 
-        # Perform a case-insensitive search for products containing the keyword in productName
+       
         query = f"SELECT * FROM Product WHERE productName LIKE '%{keyword}%'"
         cursor.execute(query)
         products = cursor.fetchall()
@@ -50,3 +50,42 @@ def get_by_search():
     except Exception as e:
         print('Error executing search query:', str(e))
         return jsonify({'success': False, 'error': 'Internal Server Error'}), 500
+    
+    # Endpoint to filter products by nationality
+@app.route('/filterByNationality', methods=['GET'])
+def filter_by_nationality():
+    try:
+        nationality = request.args.get('Nationality', '')
+
+        # Create a cursor object to interact with the database
+        cursor = myDB.cursor(dictionary=True)
+
+       
+        query = f"SELECT * FROM Product WHERE nationality = '{nationality}'"
+        cursor.execute(query)
+        products = cursor.fetchall()
+
+        # Convert the query result to a list of dictionaries for JSON serialization
+        product_list = [
+            {
+                'id': product['id'],
+                'brand': product['brand'],
+                'productImage': product['productImage'],
+                'productName': product['productName'],
+                'productPrice': float(product['productPrice']),
+                'quantity': product['quantity'],
+                'nationality': product['nationality'],
+                'discount': float(product['discount']),
+            }
+            for product in products
+        ]
+
+        cursor.close()
+
+        return jsonify({'success': True, 'products': product_list})
+
+    except Exception as e:
+        print('Error executing nationality filter query:', str(e))
+        return jsonify({'success': False, 'error': 'Internal Server Error'}), 500
+
+
