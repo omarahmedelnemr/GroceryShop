4ead54232d695e2fbb4b52ca61f89cccfd5e6bbe
Abo Nady <mahmoud.nady@ejust.edu.eg>
2023-12-11 01:57:28 +0200

    Update

--- Developers/nady.py
+++ Developers/nady.py
@@ -6,29 +6,150 @@ app = Blueprint('nady', __name__)
 
 @app.route('/getallproducts', methods=['GET'])
 def get_all_product():
+    try:
 
-    cursor = sqlcon.cursor()
-    cursor.execute("SELECT * FROM product")
-    data = cursor.fetchall()
+        cursor = sqlcon.cursor()
+        cursor.execute("SELECT * FROM product")
+        data = cursor.fetchall()
 
-    # Close the database connection
+        # Close the database connection
+        cursor.close()
+        # Return the data as a JSON response
+        return jsonify(data)
+    except:
+        response =  jsonify({'message': "Somthing went Wrong"})
+        status = 406
+
+
     cursor.close()
-    # Return the data as a JSON response
-    return jsonify(data)
+    return response,status
+
+
+@app.route('/filterbyprice', methods=['GET'])
+def fliter_by_price():
+    try:
+  
+        min= request.args.get('min')
+        max = request.args.get('max')
+        cursor = sqlcon.cursor()
+        query = f'SELECT * FROM product WHERE productPrice BETWEEN {min} AND {max};'
+
+        cursor.execute(query)
+        data = cursor.fetchall()
+        # Close the database connection
+        cursor.close()
+        # Return the data as a JSON response
+        return jsonify(data)
+    
+    except:
+        response =  jsonify({'message': "Somthing went Wrong"})
+        status = 406
+
+
+    cursor.close()
+    return response,status
+
+
+
+@app.route('/filterbybrand', methods=['GET'])
+def fliter_by_brand():
+    try:
+  
+        brand= request.args.get('brand')
+        cursor = sqlcon.cursor()
+        query = f'SELECT * FROM product INNER JOIN Brand ON product.brand = Brand.id WHERE Brand.name = {brand};'
+
+        cursor.execute(query)
+        data = cursor.fetchall()
+        # Close the database connection
+        cursor.close()
+        # Return the data as a JSON response
+        return jsonify(data)
+    
+    except:
+        response =  jsonify({'message': "Somthing went Wrong"})
+        status = 406
+
+
+    cursor.close()
+    return response,status
+
+
+
+@app.route('/addproductcart', methods=['POST'])
+def add_to_cart():
+    try:
+  
+        id = request.json.get('id')
+        prodID = request.json.get('prodID')
+        cartID = request.json.get('cartID')
+        quan = request.json.get('quan')
+
+        cursor = sqlcon.cursor()
+        query = f'INSERT INTO CartProduct (id, productID, cartID, quantity) VALUES ({id},{prodID},{cartID},{quan});'
+
+        cursor.execute(query)
+        sqlcon.commit()
+        # Close the database connection
+        cursor.close()
+        # Return the data as a JSON response
+        return jsonify('inserted Sucssefully')
+    
+    except:
+        response =  jsonify({'message': "Somthing went Wrong"})
+        status = 406
+
+
+    cursor.close()
+    return response,status
+
+
+
+@app.route('/deletefromcart', methods=['DELETE'])
+def delete_from_cart():
+    try:
+  
+        id = request.json.get('id')
+        prodID = request.json.get('prodID')
+        cartID = request.json.get('cartID')
+        quan = request.json.get('quan')
+
+        cursor = sqlcon.cursor()
+        query = f'delete from CartProduct where id = {id} and productID = {prodID} and cartID = {cartID} and quantity = {quan}; '
+
+        cursor.execute(query)
+        sqlcon.commit()
+
+        # Close the database connection
+        cursor.close()
+        # Return the data as a JSON response
+        return jsonify('Deleted Sucssefully')
+    
+    except:
+        response =  jsonify({'message': "Somthing went Wrong"})
+        status = 406
+        return response,status
 
 
 @app.route('/getproductinfo', methods=['GET'])
 def get_product_info():
-    id = request.args.get('id')
-    cursor = sqlcon.cursor()
-    query = f"SELECT * FROM product where id = {id} "
-    cursor.execute(query)
-    data = cursor.fetchall()
-
-    # Close the database connection
+    try:
+            
+        id = request.args.get('id')
+        cursor = sqlcon.cursor()
+        query = f"SELECT * FROM product where id = {id} "
+        cursor.execute(query)
+        data = cursor.fetchall()
+        # Close the database connection
+        cursor.close()
+        # Return the data as a JSON response
+        return jsonify(data)
+    
+    except:
+        response =  jsonify({'message': "Somthing went Wrong"})
+        status = 406
     cursor.close()
-    # Return the data as a JSON response
-    return jsonify(data)
+    return response,status    
 
 
 
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from flask import Flask
 
 
-from Developers.adham import app as adham
+# from Developers.adham import app as adham
 from Developers.nady import app as nady
-from Developers.nemr import app as nemr
+# from Developers.nemr import app as nemr
 app = Flask(__name__)
 
 
 # Register blueprints
-app.register_blueprint(adham, url_prefix='/')
+# app.register_blueprint(adham, url_prefix='/')
 
 app.register_blueprint(nady, url_prefix='/')
 
-app.register_blueprint(nemr, url_prefix='/')
+# app.register_blueprint(nemr, url_prefix='/')
 
 @app.route("/")
 def hello_world():
