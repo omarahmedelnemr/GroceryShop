08ee2263889ce478fd8e9be9b5da9003b662bba1
Your Nameadham-aboeldahab <adham.aboeldahab@ejust.edu.eg>
2023-12-12 21:18:44 +0200

    editing some error

--- Developers/adham.py
+++ Developers/adham.py
@@ -1,44 +1,40 @@
 from flask import Flask, jsonify, request
 import mysql.connector
 
-# app = Blueprint('adham', __name__)
-# Tempraty for Error Handle
-# MySQL configuration
-app = Flask( __name__)
-myDB = mysql.connector.connect(
-    host = 'mysql-156876-0.cloudclusters.net',
-    port = 10026,
-    user = 'admin',
-    password = '7TE9ESeh',
-    database = 'GroceryShop'    
-)
+app = Flask(__name__)
 
+# MySQL configuration
+myDB = mysql.connector.connect(
+    host='mysql-156876-0.cloudclusters.net',
+    port=10026,
+    user='admin',
+    password='7TE9ESeh',
+    database='GroceryShop'
+)
 
 # Endpoint to search products by keyword
 @app.route('/getBySearch', methods=['GET'])
 def get_by_search():
     try:
-        keyword = request.args.get('keyword', '')
+        keyword = request.args.get('keyword')
 
-       
         cursor = myDB.cursor(dictionary=True)
 
-       
-        query = f"SELECT * FROM Product WHERE productName LIKE '%{keyword}%'"
+        query = f"SELECT * FROM product WHERE productName LIKE '%{keyword}%'"
         cursor.execute(query)
         products = cursor.fetchall()
 
-        # Convert the query result to a list of dictionaries for JSON serialization
         product_list = [
             {
                 'id': product['id'],
-                'brand': product['brand'],
                 'productImage': product['productImage'],
                 'productName': product['productName'],
                 'productPrice': float(product['productPrice']),
                 'quantity': product['quantity'],
                 'nationality': product['nationality'],
+                'brand': product['brand'],
                 'discount': float(product['discount']),
+                'calories': product['calories']
             }
             for product in products
         ]
@@ -50,32 +46,30 @@ def get_by_search():
     except Exception as e:
         print('Error executing search query:', str(e))
         return jsonify({'success': False, 'error': 'Internal Server Error'}), 500
-    
-    # Endpoint to filter products by nationality
+
+# Endpoint to filter products by nationality
 @app.route('/filterByNationality', methods=['GET'])
 def filter_by_nationality():
     try:
         nationality = request.args.get('Nationality', '')
 
-        # Create a cursor object to interact with the database
         cursor = myDB.cursor(dictionary=True)
 
-       
-        query = f"SELECT * FROM Product WHERE nationality = '{nationality}'"
+        query = f"SELECT * FROM product WHERE nationality = '{nationality}'"
         cursor.execute(query)
         products = cursor.fetchall()
 
-        # Convert the query result to a list of dictionaries for JSON serialization
         product_list = [
             {
                 'id': product['id'],
-                'brand': product['brand'],
                 'productImage': product['productImage'],
                 'productName': product['productName'],
                 'productPrice': float(product['productPrice']),
                 'quantity': product['quantity'],
                 'nationality': product['nationality'],
+                'brand': product['brand'],
                 'discount': float(product['discount']),
+                'calories': product['calories']
             }
             for product in products
         ]
@@ -88,4 +82,116 @@ def filter_by_nationality():
         print('Error executing nationality filter query:', str(e))
         return jsonify({'success': False, 'error': 'Internal Server Error'}), 500
 
+# Endpoint to get payment methods for a user
+@app.route('/getPaymentMethods', methods=['GET'])
+def get_payment_methods():
+    try:
+        user_id = request.args.get('userId', '')
 
+        cursor = myDB.cursor(dictionary=True)
+
+        query = f"SELECT * FROM PayementMethods WHERE userID = '{user_id}'"
+        cursor.execute(query)
+        payment_methods = cursor.fetchall()
+
+        payment_methods_list = [
+            {
+                'id': method['id'],
+                'userID': method['userID'],
+                'cardNumber': method['cardNumber'],
+                'cvv': method['cvv'],
+                'cardHolderName': method['cardHolderName'],
+            }
+            for method in payment_methods
+        ]
+
+        cursor.close()
+
+        return jsonify({'success': True, 'paymentMethods': payment_methods_list})
+
+    except Exception as e:
+        print('Error executing getPaymentMethods query:', str(e))
+        return jsonify({'success': False, 'error': 'Internal Server Error'}), 500
+    
+    # Endpoint to add payment methods
+@app.route('/addPaymentMethods', methods=['POST'])
+def add_payment_methods():
+    try:
+        data = request.json
+
+        user_id = data.get('id', '')
+        card_number = data.get('cardNumber', '')
+        cvv = data.get('cvv', '')
+        card_holder_name = data.get('cardholderName', '')
+
+        cursor = myDB.cursor()
+        # Insert the new payment method into the PayementMethods table
+        query = "INSERT INTO payementMethods (userID, cardNumber, cvv, cardHolderName) VALUES (%s, %s, %s, %s)"
+        values = (user_id, card_number, cvv, card_holder_name)
+        cursor.execute(query, values)
+        myDB.commit()
+        cursor.close()
+
+        return jsonify({'success': True, 'message': 'Payment method added successfully'})
+    except Exception as e:
+        print('Error adding payment method:', str(e))
+        return jsonify({'success': False, 'error': 'Internal Server Error'}), 500
+
+
+
+
+
+# Endpoint to remove payment methods
+@app.route('/removePaymentMethods', methods=['DELETE'])
+def remove_payment_methods():
+    try:
+        user_id = request.args.get('userId', '')
+        payment_method_id = request.args.get('paymentMethodId', '')
+        cursor = myDB.cursor()
+        # Delete the specified payment method
+        query = "DELETE FROM PayementMethods WHERE userID = %s AND id = %s"
+        values = (user_id, payment_method_id)
+        cursor.execute(query, values)
+        myDB.commit()
+
+        cursor.close()
+
+        return jsonify({'success': True, 'message': 'Payment method removed'}), 500
+    except Exception as e:
+        # Handle the exception (you might want to log it or return an error response)
+        return jsonify({'success': False, 'message': str(e)}), 500
+    
+# Endpoint to get user's orders
+@app.route('/myOrder', methods=['GET'])
+def get_user_orders():
+    try:
+        user_id = request.args.get('userId', '')
+
+        # Create a cursor object to interact with the database
+        cursor = myDB.cursor(dictionary=True)
+
+        # Perform a search for orders associated with the user
+        query = f"SELECT * FROM `Order` WHERE userID = '{user_id}'"
+        cursor.execute(query)
+        orders = cursor.fetchall()
+
+        # Convert the query result to a list of dictionaries for JSON serialization
+        orders_list = [
+            {
+                'id': order['id'],
+                'totalPrice': float(order['totalPrice']),
+                'orderDate': order['orderDate'].isoformat(),
+                'deliveryDate': order['delevaryDate'].isoformat(),
+                'status': order['status'],
+                'userID': order['userID'],
+            }
+            for order in orders
+        ]
+
+        cursor.close()
+
+        return jsonify({'success': True, 'orders': orders_list})
+
+    except Exception as e:
+        print('Error executing get_user_orders query:', str(e))
+        return jsonify({'success': False, 'error': 'Internal Server Error'}), 500
