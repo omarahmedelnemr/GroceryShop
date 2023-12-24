580e4eb7647b6916e3e79a7c61e6bac50b779509
omarahmedelnemr <omarahmedelnemr16@gmail.com>
2023-12-24 14:00:52 +0200

    Reset Adham

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from flask import Flask
 
 
-# from Developers.adham import app as adham
+from Developers.adham import app as adham
 from Developers.nady import app as nady
 from Developers.nemr import app as nemr
+from flask_cors import CORS
+
 app = Flask(__name__)
+
+CORS(app)
 
 
 # Register blueprints
-# app.register_blueprint(adham, url_prefix='/')
+app.register_blueprint(adham, url_prefix='/')
 
 app.register_blueprint(nady, url_prefix='/')
 
@@ -19,3 +23,6 @@ def hello_world():
     return "<h1 style='text-align: center;'>Hello From Main Side</h1>"
 
 
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',debug=True)
